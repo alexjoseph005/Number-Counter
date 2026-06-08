95dedf080167075b2b481be0d2ac5dadd321a40d
Alex Joseph <alexpantha2@gmail.com>
2026-06-08 14:23:51 +0530

    fixing the time delay using in phones

--- bw.js
+++ bw.js
@@ -3,15 +3,15 @@ const resetbtn=document.getElementById("resetbtn");
 const increasebtn=document.getElementById("increasebtn");
 const countlabel=document.getElementById("countlabel");
 let count=0;
-decreasebtn.onclick=function(){
+decreasebtn.addEventListener("pointerdown", () => {
     count--;
     countlabel.textContent=count;
-}
-increasebtn.onclick=function(){
-    count++;
-    countlabel.textContent=count;
-}
-resetbtn.onclick=function(){
+});
+resetbtn.addEventListener("pointerdown", () => {
     count=0;
-    countlabel.textContent=count;  
-}
+    countlabel.textContent=count;
+});
+increasebtn.addEventListener("pointerdown", () => {
+    count++;
+    countlabel.textContent = count;
+});
